0aaed1e0d9343a2f71845228215efc711c1c34b6
高瀬英都 <>
2022-07-20 20:20:27 +0900

    コメント書いたり色々

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
-# AngularTemplate
-スタンドアローンコンポーネント使ったテンプレート, pagesの各ファイルを遅延読み込み
+# 概要
+ドット絵書く
 
-## Development server
-
-Run `yarn start` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `yarn generate component component-name` to generate a new component. You can also use `yarn generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `yarn build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `yarn test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
+1. yarn install
+1. yarn start
+1. localhost:4200/make
--- src/app/routed/make/components/dot-editor/dot-editor.component.ts
+++ src/app/routed/make/components/dot-editor/dot-editor.component.ts
@@ -19,15 +19,18 @@ export class DotEditorComponent implements OnInit{
 
   constructor() { }
 
-  // private frames: string[][][] = []
-  private pixelColors: string[][] = []
+  /** 各フレームごとのピクセルの状態 */
+  frames: string[][][] = []
 
-  // private frame = 0
+  /** 選択中のフレームのインデックス */
+  frame = 0
 
   /** ピクセル数, [縦, 横] */
   private pixels: Array2<number> = [14, 16]
 
+  /** 選択中の色 */
   picked: string = '#ffffff'
+  // なんか配列にしたらエラー出るからいっぱい変数作った
   picker1: string = '#ffffff'
   picker2: string = '#ffffff'
   picker3: string = '#ffffff'
@@ -37,6 +40,7 @@ export class DotEditorComponent implements OnInit{
   picker7: string = '#ffffff'
   picker8: string = '#ffffff'
 
+  /** マウスがクリックされた状態かどうか */
   isMouseDown: boolean = false
 
   @HostListener('mousedown', ['$event'])
@@ -52,30 +56,64 @@ export class DotEditorComponent implements OnInit{
     this.initPixelColors()
   }
 
+  /** フレームの状態を初期化する */
   initPixelColors (): void {
-    this.pixelColors = []
-    const columns = this.columns.map(() => '#FFFFFF')
-    this.rows.forEach(() => {
-      this.pixelColors.push([...columns])
-    })
+    this.frames = []
+    this.frames[0] = this.generateBlankPixels()
   }
 
+  /** 初期状態の1フレームを作る */
+  generateBlankPixels (): string[][] {
+    const pixels: string[][] = []
+    const columns = this.columns.map(() => '#FFFFFF')
+    this.rows.forEach(() => {
+      pixels.push([...columns])
+    })
+    return pixels
+  }
+
+  /** カラーピッカーで色を変えた時 */
   onColorChange (color: string): void {
     this.picked = color
   }
 
+  /** ピクセルをクリックしたときに選択中のフレームのピクセルに選択中の色を入れる */
   onClickPixel (row: number, column: number): void {
-    this.pixelColors[row][column] = this.picked
+    this.frames[this.frame][row][column] = this.picked
   }
 
+  /** マウスカーソルがピクセルの領域に入った時にマウスがクリック中ならぬる */
   onMouseEnterPixel (row: number, column: number): void {
     if (this.isMouseDown) {
-      this.pixelColors[row][column] = this.picked
+      this.frames[this.frame][row][column] = this.picked
     }
   }
 
+  /** マウスがエリアから出たときにマウスダウンフラグを下げる */
   onMouseLeaveArea (): void {
     this.isMouseDown = false
+  }
+
+  /**
+   * 選択中のフレームを変える
+   * prev: 前のフレーム
+   * next: 次のフレーム, なければ追加
+   * next-middle: 選択中のフレームと次のフレームの間にフレームを追加
+   */
+  changeFrame (direction: 'prev' | 'next' | 'next-middle'): void {
+    if (direction === 'prev' && this.frame > 0) {
+      this.frame--
+    }
+    if (direction === 'next') {
+      if (this.frame + 1 >= this.frames.length) {
+        this.frames.push(this.generateBlankPixels())
+      }
+      this.frame++
+    }
+    if (direction === 'next-middle' && this.frame + 1 < this.frames.length) {
+      this.frames.splice(this.frame + 1, 0, this.generateBlankPixels())
+      this.frame++
+    }
   }
 
   get rows (): Array<number> {
@@ -85,41 +123,54 @@ export class DotEditorComponent implements OnInit{
     return [...Array(this.pixels[1])].map((_, i) => (i))
   }
 
+  /** ピクセルの色 */
   color (row: number, column: number): string {
-    return this.pixelColors[row][column]
+    return this.frames[this.frame][row][column]
   }
 
-  get colorArray(): string {
-    const pixels = this.pixelArrayString
-    const colors: string[] = []
-    let newPixel: number[] = []
-    pixels.split(',').forEach(pixel => {
-      const i = colors.findIndex(color => color === pixel)
-      if(i < 0) {
-        colors.push(pixel)
-        newPixel.push(colors.length - 1)
-      } else {
-        newPixel.push(i)
+  /** 入力欄に入れたフレームの情報を変数に入れる */
+  set colorArray(v: string) {
+    const lines = v.split('"').join('').split(',\n').filter(v => !!v)
+    for(let i = 0; i < lines.length; i += 3) {
+      if (i >= this.frames.length) {
+        this.frames.push(this.generateBlankPixels())
       }
-    })
-    return `"${colors}",\n"${newPixel.join(',')}"`
+      this.setPixelArray(lines[i+1], lines[i+2], i / 3)
+    }
   }
 
-  set pixelArrayString(v: string) {
+  setPixelArray (paret: string, v: string, frame: number): void {
     const values = v.split(',')
-    console.log(values)
-    this.pixelColors.forEach((rows, r) => {
+    this.frames[frame].forEach((rows, r) => {
       rows.forEach((col, c) => {
         const poped = values.shift()
-        if (poped) {
-          this.pixelColors[r][c] = `#${poped}`
-        }
+        this.frames[frame][r][c] = '#' + paret.split(',')[Number(poped)].replace(/\s+/g, '')
       })
     })
-    console.log(this.pixelColors)
   }
 
-  get pixelArrayString (): string {
-    return this.pixelColors.flat().map(v => v?.replace('#', '').toLocaleUpperCase()).join(',')
+  /** フレーム情報の変数から適当にフォーマットした文字列にする */
+  get colorArray(): string {
+    let output: string = ''
+    this.frames.forEach((_frame, frameIndex) => {
+      const pixels = this.getPixelArrayString(frameIndex)
+      const colors: string[] = []
+      let newPixel: number[] = []
+      pixels.split(',').forEach(pixel => {
+        const i = colors.findIndex(color => color === pixel)
+        if(i < 0) {
+          colors.push(pixel)
+          newPixel.push(colors.length - 1)
+        } else {
+          newPixel.push(i)
+        }
+      })
+      output += `"100",\n"${colors}",\n"${newPixel.join(',')}",\n`
+    })
+    return output
+  }
+
+  getPixelArrayString (frame: number): string {
+    return this.frames[frame].flat().map(v => v?.replace('#', '').toLocaleUpperCase()).join(',')
   }
 }
